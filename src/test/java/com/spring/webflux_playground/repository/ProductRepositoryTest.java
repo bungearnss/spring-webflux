32448abe0795763707837e64cedb02c45b72b1db
bungearntun <love2554f@hotmail.com>
2025-02-20 10:50:35 +0700

    added : ProductRepository for CRUD and test

---
+++ src/test/java/com/spring/webflux_playground/repository/ProductRepositoryTest.java
@@ -0,0 +1,42 @@
+package com.spring.webflux_playground.repository;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import reactor.test.StepVerifier;
+
+@DataR2dbcTest
+public class ProductRepositoryTest {
+
+    private static final Logger log = LoggerFactory.getLogger(ProductRepositoryTest.class);
+
+    @Autowired
+    private ProductRepository repository;
+
+    @Test
+    public void findByPriceRange() {
+        this.repository.findByPriceBetween(750, 1000)
+                .doOnNext(p -> log.info("{}", p))
+                .as(StepVerifier::create)
+                .expectNextCount(3)
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    public void pageable() {
+        this.repository.findBy(PageRequest.of(0, 3).withSort(Sort.by("price").ascending()))
+                .doOnNext(p -> log.info("{}", p))
+                .as(StepVerifier::create)
+                .assertNext(p -> Assertions.assertEquals(200, p.getPrice()))
+                .assertNext(p -> Assertions.assertEquals(250, p.getPrice()))
+                .assertNext(p -> Assertions.assertEquals(300, p.getPrice()))
+                .expectComplete()
+                .verify();
+    }
+}
