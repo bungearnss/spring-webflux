22d2fab53e9966943ad0024e948374aa9396d8ad
bungearntun <love2554f@hotmail.com>
2025-02-21 10:27:53 +0700

    added : docker compose and initial data file for test

---
+++ src/main/resources/sql/customer.sql
@@ -0,0 +1,11 @@
+-- part of the docker-compose init sql.
+
+CREATE TABLE customer (
+   id SERIAL PRIMARY KEY,
+   name VARCHAR(255),
+   email VARCHAR(255)
+);
+
+INSERT INTO customer (name, email)
+SELECT 'customer' || num, 'customer' || num || '@gmail.com'
+FROM generate_series(1, 10000) AS num;
